43332db6a2f861b8f2eb58b54f41fbdf94808f63
Arunava Jana <109429100+arunavajana99@users.noreply.github.com>
2022-08-20 16:48:30 +0530

    Add files via upload

---
+++ Searching and Sorting/InsertionSort.py
@@ -0,0 +1,10 @@
+def InsertionSort(arr1,arr2):
+    i=0
+    j=0
+    len1=len(arr1)
+    len2=len(arr2)
+    arr=[]
+    while((i<len1) and (j<len2)):
+        if(arr1[i]<arr2[j])
+arr1=[9,8,5,6,7,1]
+InsertionSort(arr1,arr2)
